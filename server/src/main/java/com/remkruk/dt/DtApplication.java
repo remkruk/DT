489b849483102fcb5e5f8d16493324644caf0b91
remkruk <kruk.remigiusz@gmail.com>
2018-12-21 09:38:24 +0100

    add springBoot

---
+++ server/src/main/java/com/remkruk/dt/DtApplication.java
@@ -0,0 +1,14 @@
+package com.remkruk.dt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtApplication.class, args);
+	}
+
+}
+
---
+++ server/src/test/java/com/remkruk/dt/DtApplicationTests.java
@@ -0,0 +1,17 @@
+package com.remkruk.dt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
